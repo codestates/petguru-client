cacbd7e484a13f4829e92d0b09711ff10dddcdfe
손영산 <54147313+zeromountain@users.noreply.github.com>
2021-05-28 21:50:59 +0900

    :construction: 5.login
    이메일 로그인 구현했으나 리팩토링이 필요해보임

--- actions/user_actions.js
+++ actions/user_actions.js
@@ -5,10 +5,7 @@ import { LOGIN_USER, REGISTER_USER } from "./types";
 
 export function loginUser(dataToSubmit) {
   const req = axios
-    .post(
-      "https://1c045875-3182-4e7d-b64d-8ab15c69cea2.mock.pstmn.io/user/login",
-      dataToSubmit,
-    )
+    .post("http://localhost:3065/user/login", dataToSubmit) // URL: 배포 서버랑 연결해야하고 URL 변수 만들어서 defualt URL 관리
     .then((res) => res.data);
 
   return {
@@ -19,7 +16,7 @@ export function loginUser(dataToSubmit) {
 
 export function registerUser(dataToSubmit) {
   const req = axios
-    .post("http://localhost:3065/user/register", dataToSubmit)
+    .post("http://localhost:3065/user/register", dataToSubmit) // URL: 배포 서버랑 연결해야함 URL 변수 만들어서 defualt URL 관리
     .then((res) => res.data);
 
   return {
