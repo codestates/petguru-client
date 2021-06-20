0706812fca850aedc2d2c4099cf65e69b7df3fcd
손영산 <54147313+zeromountain@users.noreply.github.com>
2021-06-20 19:47:17 +0900

    :bug: 로그인 실행 점검
    회원가입 후에 저장된 auth로 로그인 되는 부분 해결해야함

--- redux/lib/api/auth.js
+++ redux/lib/api/auth.js
@@ -1,10 +1,14 @@
 import client from './client';
 
 // 로그인
-export const login = ({ email, password }) => client.post('/user/login');
+export const login = ({ email, password }) => client.post('/user/login', { email, password });
+// export const login = async ({ email, password }) => {
+//   const response = client.post('/user/login', { email, password });
+//   console.log('서버로부터 받은 응답: ' ,response);
+// }
 
 // 구글 로그인
-export const googleLogin = ({email, name, googleId}) => client.post('/user/oauth');
+export const googleLogin = ({email, name, googleId}) => client.post('/user/auth/google');
 
 export const register = ({ username, email, password }) => client.post('/user/signup', { username, email, password });
 
--- redux/lib/createRequestActionTypes.js
+++ redux/lib/createRequestActionTypes.js
@@ -13,13 +13,20 @@ export default function createRequestSaga(type, request) {
 
   return function*(action) {
     yield put(startLoading(type)); // 로딩 시작
+    console.log('로딩이 시작됩니다.')
+    console.log(action)
     try {
+      console.log('action은', action);
+      console.log(request);
+      console.log('try문이 시작됩니다.')
       const response = yield call(request, action.payload);
+      console.log('response', response);
       yield put({
         type: SUCCESS,
         payload: response.data,
         meta: response,
       });
+      console.log('put까지 완료')
     } catch (e) {
       yield put({
         type: ERROR,
