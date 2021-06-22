a9af9e71cd4a5ea19e056b2716efa1fb5194dc69
손영산 <54147313+zeromountain@users.noreply.github.com>
2021-06-22 14:19:23 +0900

    :bug: 게시물 수정 기능 오류 수정 보완

--- redux/modules/missing_write.js
+++ redux/modules/missing_write.js
@@ -1,52 +1,54 @@
-import { takeLatest } from '@redux-saga/core/effects';
-import { createAction, handleActions } from 'redux-actions';
-import * as postsAPI from '../lib/api/missing/posts';
-import createRequestSaga, { createRequestActionTypes } from '../lib/createRequestActionTypes';
+import { takeLatest } from "@redux-saga/core/effects";
+import { createAction, handleActions } from "redux-actions";
+import * as postsAPI from "../lib/api/missing/posts";
+import createRequestSaga, {
+  createRequestActionTypes,
+} from "../lib/createRequestActionTypes";
 
-const INITIALIZE = 'write/INITIALIZE';
-const CHANGE_FIELD = 'write/CHANGE_FIELD';
-const SET_ORIGINAL_POST = 'wrtie/SET_ORIGINAL_POST';
+const INITIALIZE = "write/INITIALIZE";
+const CHANGE_FIELD = "write/CHANGE_FIELD";
+const SET_ORIGINAL_POST = "wrtie/SET_ORIGINAL_POST";
 
 export const initialize = createAction(INITIALIZE);
-export const changeField = createAction(CHANGE_FIELD, ({ key, value }) => ({ key, value }));
-export const setOriginalPost = createAction(SET_ORIGINAL_POST, post => post);
-
-const [
-  WRITE_POST,
-  WRITE_POST_SUCCESS,
-  WRITE_POST_ERROR
-] = createRequestActionTypes('write/WRITE_POST'); // 게시물 작성
-const [
-  UPDATE_POST,
-  UPDATE_POST_SUCCESS,
-  UPDATE_POST_ERROR
-] = createRequestActionTypes('write/UPDATE_POST'); // 게시물 수정
-
-export const writePost = createAction(WRITE_POST, ({
-  // formData
-  contents,
-  latitude,
-  longitude,
-  location,
-  image_url,
-  missing_date,
-  born_year,
-  pet_name,
-  type,
-  sex
-}) => ({
-  // formData
-  contents,
-  latitude,
-  longitude,
-  location,
-  image_url,
-  missing_date,
-  born_year,
-  pet_name,
-  type,
-  sex,
+export const changeField = createAction(CHANGE_FIELD, ({ key, value }) => ({
+  key,
+  value,
 }));
+export const setOriginalPost = createAction(SET_ORIGINAL_POST, (post) => post);
+
+const [WRITE_POST, WRITE_POST_SUCCESS, WRITE_POST_ERROR] =
+  createRequestActionTypes("write/WRITE_POST"); // 게시물 작성
+const [UPDATE_POST, UPDATE_POST_SUCCESS, UPDATE_POST_ERROR] =
+  createRequestActionTypes("write/UPDATE_POST"); // 게시물 수정
+
+export const writePost = createAction(
+  WRITE_POST,
+  ({
+    // formData
+    contents,
+    latitude,
+    longitude,
+    location,
+    image_url,
+    missing_date,
+    born_year,
+    pet_name,
+    type,
+    sex,
+  }) => ({
+    // formData
+    contents,
+    latitude,
+    longitude,
+    location,
+    image_url,
+    missing_date,
+    born_year,
+    pet_name,
+    type,
+    sex,
+  }),
+);
 export const updatePost = createAction(
   UPDATE_POST,
   ({
@@ -54,7 +56,7 @@ export const updatePost = createAction(
     title,
     contents,
     latitude,
-    logitude,
+    longitude,
     location,
     image_url,
     missing_date,
@@ -67,7 +69,7 @@ export const updatePost = createAction(
     title,
     contents,
     latitude,
-    logitude,
+    longitude,
     location,
     image_url,
     missing_date,
@@ -75,8 +77,8 @@ export const updatePost = createAction(
     pet_name,
     type,
     sex,
-  })
-)
+  }),
+);
 
 // saga
 const writePostSaga = createRequestSaga(WRITE_POST, postsAPI.writePost);
@@ -87,31 +89,30 @@ export function* writeSaga() {
   yield takeLatest(UPDATE_POST, updatePostSaga);
 }
 
-
 const initialState = {
-  contents: '',
-  latitude: '',
-  longitude: '',
-  location: '',
-  image_url: '',
-  missing_date: '',
-  born_year: '',
-  pet_name: '',
-  type: '',
-  sex: '',
+  contents: "",
+  latitude: "",
+  longitude: "",
+  location: "",
+  image_url: "",
+  missing_date: "",
+  born_year: "",
+  pet_name: "",
+  type: "",
+  sex: "",
   post: null,
   postError: null,
   originalPostId: null,
-}
+};
 
 const write = handleActions(
   {
-    [INITIALIZE]: state => initialState, // initialState를 넣으면 초기 상태로 변경
+    [INITIALIZE]: (state) => initialState, // initialState를 넣으면 초기 상태로 변경
     [CHANGE_FIELD]: (state, { payload: { key, value } }) => ({
       ...state,
       [key]: value,
     }),
-    [WRITE_POST]: state => ({
+    [WRITE_POST]: (state) => ({
       ...state,
       // post와 postError를 초기화
       post: null,
@@ -129,8 +130,17 @@ const write = handleActions(
     }),
     [SET_ORIGINAL_POST]: (state, { payload: post }) => ({
       ...state,
-      post,
-      orginalPostId: post.id,
+      contents: post.missingInfo.contents,
+      latitude: post.missingInfo.latitude,
+      longitude: post.missingInfo.longitude,
+      location: post.missingInfo.location,
+      image_url: post.missingInfo.image_url,
+      missing_date: post.missingInfo.missing_date,
+      born_year: post.missingInfo.born_year,
+      pet_name: post.missingInfo.pet_name,
+      type: post.missingInfo.type,
+      sex: post.missingInfo.sex,
+      orginalPostId: post.missingInfo.id,
     }),
     [UPDATE_POST_SUCCESS]: (state, { payload: post }) => ({
       ...state,
